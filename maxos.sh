d2d557dad6856f78411ac4be2c3f855c9b8a6d93
ubuntu2310fake <152691685+ubuntu2310fake@users.noreply.github.com>
2024-01-13 21:53:32 +0700

    Create maxos.sh

---
+++ maxos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget -O downloadmaxos.sh https://raw.githubusercontent.com/ubuntu2310fake/maxospreinstallforcs/main/createmaxos.sh
+chmod +x downloadmaxos.sh
+bash downloadmaxos.sh
